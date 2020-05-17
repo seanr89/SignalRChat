d5d7d92330dfba0e4ea2a4df3c8609335c705658
Sean <srafferty89@gmail.com>
2020-05-17 20:44:00 +0100

    Updated comments

--- src/SignalServer/ChatHub.cs
+++ src/SignalServer/ChatHub.cs
@@ -8,6 +8,13 @@ namespace SignalServer
 {
     public class ChatHub : Hub
     {
+        /// <summary>
+        /// Supports messages being received from external sources
+        /// Handles then the broadcasting of that message to all others
+        /// </summary>
+        /// <param name="name">App or Users Name</param>
+        /// <param name="message">The string content to send across</param>
+        /// <returns>a broadcastMessage to others listening</returns>
         public async Task SendMessage(string name, string message)
         {
             await Clients.All.SendAsync("broadcastMessage", name, message);
